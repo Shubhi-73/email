84d0595ea2d5258460169c3c7680ad87640332c6
unknown <srivastava.snigdha519@gmail.com>
2024-02-20 20:08:21 +0530

    removed redundant googleAPI code

--- EmailJS.js
+++ EmailJS.js
@@ -4,38 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
-const nodemailer = require("nodemailer")
-const {google} = require("googleapis")
+const nodemailer = require("nodemailer");
 const axios = require('axios');
 require('dotenv').config();
-
-//const JSONStream = require('JSONStream');
-
-const CLIENT_ID = process.env.CLIENT_ID;     
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-const REFRESH_TOKEN = process.env.REFRESH_TOKEN;
-
-////////////////////////////////////////////////////////////////
-
-
-// const oAuth2Client = new google.auth.OAuth2();
-// const serviceAccountCredentials = require('./google.json');
-// oAuth2Client.setCredentials(serviceAccountCredentials);
-// console.log("THis is the service account CREDENTIALS");
-
-// console.log(serviceAccountCredentials.client_email);
-
-
-
-
-// //passing the credentials to the gmail api to fetch the refresh token
-const oAuth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URI
-);
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 //establishing a connection to the mongoDB database
 mongoose.connect(process.env.MONGODB_URI, {
@@ -114,59 +85,6 @@ function saveQuote(foundNote,userName){
         newQuote.save();
    }
            
-       
-
-async function sendMail(mailId,foundNote)
-          {
-            try
-            {
-                const accessToken = await oAuth2Client.getAccessToken();
-
-                // const transport = nodemailer.createTransport(
-                //   {
-                //       service: 'gmail',
-                //       auth:
-                //       {
-                //           type: 'OAuth2',
-                //           user: serviceAccountCredentials.client_email,
-                //           serviceClient: serviceAccountCredentials.client_id,
-                //           privateKey: serviceAccountCredentials.private_key,
-                    
-                //       },
-                //   });
-
-                const transport = nodemailer.createTransport(
-                {
-                    service: 'gmail',
-                    auth:
-                    {
-                        type: 'OAuth2',
-                        user: 'srivastava.snigdha519@gmail.com',
-                        clientId: CLIENT_ID,
-                        clientSecret: CLIENT_SECRET,
-                        refreshToken: REFRESH_TOKEN,
-                        accessToken: accessToken,
-                    },
-                });
-
-                const mailOptions =
-                {
-                  from: 'Readwise <srivastava.snigdha519@gmail.com>',
-                  to: mailId,
-                  subject: 'Quote of the day!',
-                  text: 'Hello from the other side',
-                  html: '<div style="background-color: #95D1CC;text-align: center;padding: 1em; border-radius: 25% 10%; font: Roboto;">'+'<h2>'+foundNote[0].book+'</h2><h3 style="font-style: italic;">'+foundNote[0].content+'</h3></div>'
-                }
-
-                const result = await transport.sendMail(mailOptions);
-                console.log("reached till send mail");
-                return result;
-            } catch (error)
-            {
-              console.log(error);
-              return error;
-            }
-          } //sendMail()
           
           app.get('/run', async (req, res) => {
             try {
@@ -212,32 +130,15 @@ async function sendMail(mailId,foundNote)
                             'Content-Type': 'application/json'
                         }
                     });
-                    console.log("THIS IS THE RESPONSE")
-                    console.log(response);
+                 
                 }
                 // You can send a success response back to the client
                 res.send('Your mail is sent!');
             } catch (error) {
                 console.error(error);
                 res.status(500).send('An error occurred while processing your request.');
-      
-                  
-                // } catch (error) {
-                //     // Handle error if request fails
-                
-                //     res.status(400).json({ error: 'Oops... Something went wrong.' });
-                // }
-          
-          
-                        // sendMail(userEmail,foundNote) //returns a promise
-                        // .then((result) => console.log('Email sent...', result)) //consuming function
-                        // .catch((error) => console.log(error.message));
-                      
-                 
-          
-                    
-                    }     
-                  
+                 }     
+             
                  
           });
    
